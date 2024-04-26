d14741e9621b9cbdc6c44e7fc8f0b3b4d568e4d8
RifaOrin <rifaorin1234@gmail.com>
2024-04-26 11:56:21 +0600

    otp activity added

---
+++ app/src/main/java/com/example/mydoctorapp/otp_phone.java
@@ -0,0 +1,14 @@
+package com.example.mydoctorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class otp_phone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.otp_phone);
+    }
+}
