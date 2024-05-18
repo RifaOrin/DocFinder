7410e1baa4f00785dcf1d4e880162b70eae9d9f4
Kasif Hasnaen Zisan <zisan321@gmail.com>
2024-05-18 23:04:05 +0600

    Login OTP function added

--- app/src/main/java/com/example/mydoctorapp/OtpPhoneActivity.java
+++ app/src/main/java/com/example/mydoctorapp/OtpPhoneActivity.java
@@ -1,23 +1,52 @@
 package com.example.mydoctorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
-
-
+import android.widget.Toast;
 
 public class OtpPhoneActivity extends AppCompatActivity {
+
+    EditText etPhone;
+    Button btnNext, btnLoginPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_phone);
+        termsAndConditions();
+
+        etPhone = findViewById(R.id.etPhone);
+        btnNext = findViewById(R.id.btnNext);
+        btnLoginPass = findViewById(R.id.btnLoginPass);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = etPhone.getText().toString().trim();
+                if (phoneNumber.length() == 10 && phoneNumber.startsWith("1")) {
+                    String fullPhoneNumber = "+880" + phoneNumber;
+                    Intent intent = new Intent(OtpPhoneActivity.this, OtpActivity.class);
+                    intent.putExtra("phone", fullPhoneNumber);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(OtpPhoneActivity.this, "Invalid Phone Number", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    void termsAndConditions(){
         TextView termsTextView = findViewById(R.id.termsTextView);
 
         String text = "You agree to our Terms, Conditions and Privacy Policy";
