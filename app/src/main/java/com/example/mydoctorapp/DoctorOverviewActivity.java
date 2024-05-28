954f180e976b350321ecf6a3e031e7dea7a5110f
Kasif Hasnaen Zisan <zisan321@gmail.com>
2024-05-28 20:28:07 +0600

    Profile Page Implemented

--- app/src/main/java/com/example/mydoctorapp/DoctorOverviewActivity.java
+++ app/src/main/java/com/example/mydoctorapp/DoctorOverviewActivity.java
@@ -1,14 +1,21 @@
 package com.example.mydoctorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class DoctorOverviewActivity extends AppCompatActivity {
+    private AppCompatImageButton backToDoctorProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_overview);
+        backToDoctorProfile = findViewById(R.id.backButton);
+        backToDoctorProfile.setOnClickListener(v -> {
+            finish();
+        });
     }
 }
