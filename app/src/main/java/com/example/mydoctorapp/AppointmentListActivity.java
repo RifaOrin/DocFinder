617731d3fe4a7973c8d43c811d1e58b3aef2f8c6
RifaOrin <rifaorin1234@gmail.com>
2024-05-28 13:25:15 +0600

    navbar, more page added

--- app/src/main/java/com/example/mydoctorapp/AppointmentListActivity.java
+++ app/src/main/java/com/example/mydoctorapp/AppointmentListActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ProfilePageActivity extends AppCompatActivity {
+public class AppointmentListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_page);
+        setContentView(R.layout.activity_appointment_list);
     }
 }
